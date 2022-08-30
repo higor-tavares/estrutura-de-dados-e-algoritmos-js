03b9a46fc1aec1088ef00c553e22df0f94a88cee
paulo.tavares <paulo.tavares@olxbr.com>
2022-08-30 07:23:01 -0300

    Finishing queue data structure

---
+++ queue/exemplo-1-cap5.js
@@ -0,0 +1,10 @@
+const {Queue} = require('./queue-objeto')
+const filaDoPao = new Queue();
+filaDoPao.enqueue("Marcos");
+filaDoPao.enqueue("Jose");
+filaDoPao.enqueue("Dona maria");
+filaDoPao.enqueue("Seu araújo");
+console.log("O PÃO FICOU PRONTO!!!");
+while(!filaDoPao.isEmpty()){
+    console.log(`Atendendo o client ${filaDoPao.dequeue()}`);
+}
--- queue/queue-objeto.js
+++ queue/queue-objeto.js
@@ -4,4 +4,41 @@ class Queue{
         this.__lowest = 0;
         this.__items = {}
     }
+
+    isEmpty(){
+        return this.__length === this.__lowest;
+    }
+
+    size(){
+        return this.__length - this.__lowest;
+    }
+
+    enqueue(element){
+        this.__items[this.__length] = element;
+        this.__length ++;
+    }
+    dequeue(){
+        if(this.isEmpty()){
+            return undefined;
+        }
+        const element = this.__items[this.__lowest];
+        delete this.__items[this.__lowest];
+        this.__lowest ++;
+        return element;
+
+    }
+    peek(){
+        if(this.isEmpty()){
+            return undefined;
+        }
+        return this.__items[this.__lowest];
+    }
+    clear(){
+        this.__length = 0;
+        this.__lowest = 0;
+        this.__items = {}
+    }
+}
+module.exports = {
+    Queue
 }
